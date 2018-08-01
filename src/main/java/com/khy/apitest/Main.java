6755e7ee9bc9ce388f11c9b42259ddab109fce8f
mogutang <15088749217@163.com>
2018-08-01 10:43:56 +0800

    账号可配置

--- src/main/java/com/khy/apitest/Main.java
+++ src/main/java/com/khy/apitest/Main.java
@@ -3,7 +3,7 @@ package com.khy.apitest;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath="D:\\apache-jmeter-3.3\\bin\\resources\\dev_account.csv";
+        String filePath="D:\\apache-jmeter-3.3\\resources\\dev_account.csv";
         int a=new GetFileData().getFileRow(filePath);
         System.out.println(a);
         String b=new GetFileData().readTxtLine(filePath,3);
